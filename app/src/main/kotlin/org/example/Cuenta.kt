293f94d31ed2f555d34742d22ba8f6b13937da7e
JuanIgnacioCruzado <j.cruzado@alumno.etec.um.edu.ar>
2025-08-26 13:03:00 -0300

    avances issue 1

---
+++ app/src/main/kotlin/org/example/Cuenta.kt
@@ -0,0 +1,11 @@
+package org.example
+
+class Cuenta(
+    val numeroCuenta: String,
+    private var saldo: Double
+    val propietario: Usuario
+) {
+    fun obtenerSaldo(): Double {
+        return saldo
+    }
+}
